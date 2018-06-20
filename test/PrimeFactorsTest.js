4637bfde4dce73b73db7b381cb959de4dca2f69b
ky-yk-d <key0of7one0s5life@gmail.com>
2018-06-21 08:55:06 +0900

    説明を追加

--- test/PrimeFactorsTest.js
+++ test/PrimeFactorsTest.js
@@ -1,3 +1,13 @@
+// Factorize a positive integer number into its prime factors.
+
+// 	 2 -> [2]
+// 	 3 -> [3]
+// 	 4 -> [2,2]
+// 	 6 -> [2,3]
+// 	 9 -> [3,3]
+// 	12 -> [2,2,3]
+// 	15 -> [3,5]
+
 'use strict';
 const assert = require('power-assert');
 const PrimeFactors = require('../src/PrimeFactors');
