8268078f84bde5341b5aef1898969bacadcee735
ky-yk-d <key0of7one0s5life@gmail.com>
2018-06-22 06:54:42 +0900

    答えは出たっぽい。リファクタしたい。

--- src/PrimeFactors.js
+++ src/PrimeFactors.js
@@ -1,7 +1,30 @@
 'use strict';
 
 exports.Solver = function(){
-  this.factorize = function (num) {
-    return 2;
+
+  this.findSmallestFactor = function(num){
+    let possibleLargestFactor = Math.floor(Math.sqrt(num));
+    for (let i = 2; i <= possibleLargestFactor; i++){
+      if (num % i === 0){
+        return i;
+      }
+    }
+    return 0;
+  };
+
+  this.factorize = function(num){
+    let result = [];
+    let temp = num;
+    while (true){
+      let smallestFactor = this.findSmallestFactor(temp);
+      if (smallestFactor === 0 ){
+        result.push(temp);
+        break;
+      } else {
+        result.push(smallestFactor);
+        temp = temp / smallestFactor;
+      }
+    }
+    return result;
   };
 };
--- test/PrimeFactorsTest.js
+++ test/PrimeFactorsTest.js
@@ -17,4 +17,26 @@ describe('PrimeFactors', () => {
   it ('2のときは2を返す', () => {
     assert(solver.factorize(2).toString() === [2].toString());
   });
+  it ('4のときは[2,2]を返す', () => {
+    let solver = new PrimeFactors.Solver();
+    assert(solver.factorize(4).toString() === [2,2].toString());
+  });
+  it ('6のときは[2,3]を返す', () => {
+    let solver = new PrimeFactors.Solver();
+    assert(solver.factorize(6).toString() === [2,3].toString());
+  }); 
+  it ('108のときは[2,2,3,3,3]を返す', () => {
+    let solver = new PrimeFactors.Solver();
+    assert(solver.factorize(108).toString() === [2,2,3,3,3].toString());
+  });
+  describe('最小の約数を探す', () => {
+    it ('12を渡すと2を返す', () => {
+      let solver = new PrimeFactors.Solver();
+      assert(solver.findSmallestFactor(12) === 2);
+    });
+    it ('17を渡すと0を返す', () => {
+      let solver = new PrimeFactors.Solver();
+      assert(solver.findSmallestFactor(17) === 0);
+    });
+  });
 });
